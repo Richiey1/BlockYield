2e5ab7924d7d2f4f13c53fe97f8f4ea904e66d36
Richiey1 <ojodamilarey@gmail.com>
2026-05-25 02:40:51 +0100

    fix: correct contract name to blockbet-beta

--- frontend/lib/constants/contracts.ts
+++ frontend/lib/constants/contracts.ts
@@ -1,6 +1,6 @@
 // Standardized Contract Constants for BlockBet
 export const DEPLOYER_ADDRESS = 'SP258BY8D71JCTV73A4V3ADPHCVWSBEM6G4FETPYF';
-export const CONTRACT_NAME = 'blockbets-beta';
+export const CONTRACT_NAME = 'blockbet-beta';
 export const CONTRACT_FULL_NAME = `${DEPLOYER_ADDRESS}.${CONTRACT_NAME}`;
 
 export const FUNCTION_NAMES = {
